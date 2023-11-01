dfa1f06cc237b59d62ace42409e7715df6c468c8
1ezzy <ezra.furry@gmail.com>
2023-11-01 15:35:40 -0400

    added some images Pog

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
 <div class="container h-full mx-auto flex justify-center items-center">
-	<h1 class="flex items-center justify-center">Site coming soon...</h1>
+	<div class="flex flex-col gap-4 items-center">
+		<img src="/images/iconn.png" alt="ezzy" class="h-36 w-36" />
+		<h1 class="flex items-center justify-center text-2xl text-primary-500">Site coming soon!</h1>
+	</div>
 </div>
