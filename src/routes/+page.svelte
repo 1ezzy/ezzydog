7f4761c7d3d4efd9741a9e4838d63ec1a09ea7e0
1ezzy <ezra.furry@gmail.com>
2024-07-12 13:46:27 -0400

    fuck it we ball

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,81 +7,99 @@
 	import photos from '$lib/images/photos.png';
 </script>
 
-<main class="h-full w-full p-10 flex md:flex-row flex-col flex-1">
-	<div class="p-4 flex flex-col items-center justify-center basis-1/2">
-		<p class="text-xl tracking-wider">welcome to</p>
-		<h1 class="text-5xl tracking-wider mb-8">ezzy.dog</h1>
+<main class="w-full md:p-10 p-2 flex flex-col flex-1 gap-8">
+	<section class="h-[calc(100dvh_-_40px)] flex md:flex-row flex-col items-center justify-center">
 		<img
-			class="w-full lg:max-w-lg max-w-4xl aspect-square"
+			class="w-full md:max-w-lg max-w-1/2 aspect-square md:basis-1/2"
 			src={guitar}
 			alt="ezzy playing guitar"
 		/>
-	</div>
-	<div class="md:w-16"></div>
-	<div class="p-4 flex flex-col items-center basis-1/2 md:overflow-y-auto">
-		<h1 class="lg:text-4xl text-5xl tracking-wider underline mb-16">my projects</h1>
-		<div class="w-full flex-1">
-			<div class="project mb-16">
-				<div class="flex md:flex-row flex-col md:justify-between justify-center mb-8">
+		<div
+			class="md:h-2/3 h-1/2 flex flex-col items-center justify-center basis-1/2 md:gap-16 gap-10"
+		>
+			<div>
+				<p class="text-start text-[#ffb1b1] leading-3">welcome to</p>
+				<h1 class="text-5xl tracking-wider text-[#ffb1b1] underline decoration-wavy">ezzy.dog</h1>
+			</div>
+			<div class="gap-4 md:text-center text-start flex flex-col items-center">
+				<p class="w-2/3 tracking-wide leading-relaxed">
+					Hey there! I'm Ezra, a web developer passionate about all things front-end and full-stack.
+				</p>
+				<p class="w-2/3 tracking-wide leading-relaxed">
+					Below you can find some of the projects I've worked on and some of the technologies I love
+					using in my stack. Thanks for checking me out!
+				</p>
+			</div>
+		</div>
+	</section>
+	<section class="md:p-8 w-4/5 mx-auto flex flex-col items-center">
+		<h1 class="mb-8 text-5xl tracking-wider text-[#ffb1b1] underline decoration-wavy">projects</h1>
+		<div class="w-full flex flex-col flex-1 gap-16">
+			<div class="project">
+				<div class="flex md:flex-row flex-col md:justify-between justify-center">
 					<div class="flex flex-col">
-						<h3 class="md:text-3xl text-2xl md:mb-0 mb-4">cauldron</h3>
+						<h3 class="md:text-3xl text-2xl md:mb-0 mb-4">Cauldron</h3>
 					</div>
-					<div class="flex gap-4">
+					<div class="flex md:flex-row flex-col gap-4">
 						<button class="nav-btn md:text-md text-sm">Go To Website</button>
 						<button class="md:ml-auto nav-btn md:text-md text-sm">View on GitHub</button>
 					</div>
 				</div>
-				<div class="flex justify-between mb-4">
+				<p class="md:text-xl text-md">
+					Cauldron is an open source platform to explore, learn, and save collections of 5e spells.
+				</p>
+				<div class="flex md:flex-row flex-col justify-between gap-4">
 					<img
-						class="m-2 basis-1/3 w-1/3 aspect-square rounded-xl"
+						class="basis-1/3 md:w-1/3 w-full aspect-square rounded-xl"
 						src={alarm}
 						alt="spell details"
 					/>
 					<img
-						class="m-2 basis-1/3 w-1/3 aspect-square rounded-xl"
+						class="basis-1/3 md:w-1/3 w-full aspect-square rounded-xl"
 						src={spellbook}
 						alt="spellbook view"
 					/>
-					<img class="m-2 basis-1/3 w-1/3 aspect-square rounded-xl" src={cards} alt="spell table" />
+					<img
+						class="basis-1/3 md:w-1/3 w-full aspect-square rounded-xl"
+						src={cards}
+						alt="spell table"
+					/>
 				</div>
-				<p class="md:text-xl text-md">
-					cauldron is an open source platform to explore, learn, and save collections of 5e spells.
-				</p>
 			</div>
 			<div class="project">
-				<div class="flex md:flex-row flex-col justify-between mb-8">
+				<div class="flex md:flex-row flex-col justify-between">
 					<div class="flex flex-col">
-						<h3 class="md:text-3xl text-2xl md:mb-0 mb-4">the paper earth</h3>
+						<h3 class="md:text-3xl text-2xl md:mb-0 mb-4">The Paper Earth</h3>
 					</div>
 					<div class="flex gap-4">
 						<button class="nav-btn md:text-md text-sm">Go To Website</button>
 						<button class="md:ml-auto nav-btn md:text-md text-sm">View on GitHub</button>
 					</div>
 				</div>
-				<div class="flex justify-between mb-4">
+				<p class="md:text-xl text-md">The Paper Earth is an alt-punk band from Cincinnati, OH!</p>
+				<div class="flex md:flex-row flex-col justify-between gap-4">
 					<img
-						class="ml-2 mr-4 basis-2/3 w-2/3 aspect-[2/1] rounded-xl"
+						class="basis-2/3 md:w-2/3 w-full aspect-[2/1] rounded-xl"
 						src={band}
 						alt="band splash page"
 					/>
 					<img
-						class="mr-2 basis-1/3 w-1/3 aspect-square rounded-xl"
+						class="basis-1/3 md:w-1/3 w-full aspect-square rounded-xl"
 						src={photos}
 						alt="band photos"
 					/>
 				</div>
-				<p class="md:text-xl text-md">
-					website for the paper earth, an alt-punk band from cincinnati, OH!
-				</p>
 			</div>
 		</div>
-	</div>
+	</section>
 </main>
 
 <style lang="postcss">
 	.project {
 		width: 100%;
-		max-width: 1024px;
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
 	}
 
 	.nav-btn {
