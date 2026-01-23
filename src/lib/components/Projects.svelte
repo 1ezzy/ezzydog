5a19bff659daed3eb03f3ba994ce165aac893269
1ezzy <ezra.furry@gmail.com>
2026-01-22 19:06:43 -0500

    minor cleanup to prep projects section

--- src/lib/components/Projects.svelte
+++ src/lib/components/Projects.svelte
@@ -8,11 +8,11 @@
 	const xlarge = new MediaQuery('min-width: 2560px');
 </script>
 
-<div class="flex min-h-screen w-full flex-col p-16 pt-48 3xl:p-32 3xl:pt-96">
+<div class="flex min-h-screen w-full flex-col p-16 pt-0 3xl:p-32">
 	<div class="mb-16 flex flex-row items-center justify-between">
-		<h2 id="projects" class="pt-16 text-6xl">My Projects</h2>
+		<h2 id="projects" class="pt-16 text-4xl">My Projects</h2>
 	</div>
-	<div class="flex h-screen items-center justify-center">
+	<div class="flex h-full items-center justify-center">
 		<h2 class="text-4xl">Coming soon...</h2>
 	</div>
 </div>
